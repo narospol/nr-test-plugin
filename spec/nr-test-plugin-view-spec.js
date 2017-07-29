4ab5df2b49eddf569c4b6e112b882f31f3ff3cf8
narospol pathong <naros.pol@gmail.com>
2017-07-29 21:40:54 +0700

    initialze testing plugin

---
+++ spec/nr-test-plugin-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import NrTestPluginView from '../lib/nr-test-plugin-view';
+
+describe('NrTestPluginView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
